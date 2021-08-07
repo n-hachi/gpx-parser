1d6ba24946da01ab76d3a6425dfc6afd0e188d33
Nozomu Hatta <nozomuhatta@gmail.com>
2021-08-07 15:39:18 +0900

    Implement minimum test for Gpx constructor
    In current state, it is correct that the build process fails.

--- test/gpx-parser_test.cc
+++ test/gpx-parser_test.cc
@@ -1,3 +1,20 @@
+#include <sstream>
+
+#include "gpx/gpx.h"
 #include "gtest/gtest.h"
 
-TEST(sample_test_case, sample_test) { EXPECT_EQ(1, 1); }
+/*
+ * Tests for Gpx class constructor with minimam string.
+ */
+TEST(GpxTest, GpxMinimumTest) {
+    std::istringstream ss(
+        u8R"##(
+<gpx
+ version="1.1"
+ creator="n-hachi">
+</gpx>
+)##");
+    gpx::Gpx gpx(ss);
+    EXPECT_EQ(gpx.version(), "1.1");
+    EXPECT_EQ(gpx.creator(), "n-hachi");
+}
