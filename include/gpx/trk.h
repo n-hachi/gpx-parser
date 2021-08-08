eb6d5f46120b8b1bd0ee58b12c2b5151ff52d296
Nozomu Hatta <nozomuhatta@gmail.com>
2021-08-08 23:34:39 +0900

    Add base of Trk class

---
+++ include/gpx/trk.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+
+namespace gpx {
+
+/**
+ * Reference
+ * https://www.topografix.com/GPX/1/1/#type_trkType
+ */
+class Trk {
+   public:
+   private:
+};
+}  // namespace gpx
---
+++ src/trk.cc
@@ -0,0 +1,1 @@
+#include "gpx/trk.h"
