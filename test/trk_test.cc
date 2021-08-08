2185942446ba9f51772d0b9f9347266e873985da
Nozomu Hatta <nozomuhatta@gmail.com>
2021-08-09 00:32:55 +0900

    Add test for Trk class constructor with unproper argment string
    Trk class need to be received string which has 'trk' string with
    top level label.

--- test/trk_test.cc
+++ test/trk_test.cc
@@ -27,3 +27,26 @@ TEST(TrkTest, TrkMinimumTest) {
     // Create trk
     EXPECT_NO_THROW((gpx::Trk(node)));
 }
+
+/*
+ * Tests for Trk class constructor with unproper string.
+ */
+TEST(TrkTest, TrkUnproperArgumentTest) {
+    // Sample input
+    std::istringstream ss(
+        u8R"##(
+<unproper_tag>
+</unproper_tag>
+)##");
+
+    /* Create xml_node */
+    std::istreambuf_iterator<char> it{ss};
+    std::vector<char> buf(it, std::istreambuf_iterator<char>());
+    buf.push_back('\0');
+    rapidxml::xml_document<> doc;
+    doc.parse<0>(&buf[0]);
+    rapidxml::xml_node<> *node = doc.first_node();
+
+    // Create trk
+    EXPECT_THROW((gpx::Trk(node)), std::invalid_argument);
+}
